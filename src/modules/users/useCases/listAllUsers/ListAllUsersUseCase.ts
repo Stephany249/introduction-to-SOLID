3af5d5b55fb6c7e194fec3779ca1d0bad009e458
Stephany <tete.1818@hotmail.com>
2022-01-10 15:08:18 -0300

    challenge adjustments

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
   user_id: string;
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userId = this.usersRepository.findById(user_id);
+
+    if (!userId) {
+      throw new Error('Usuario não encontrado');
+    }
+
+    if (!userId.admin) {
+      throw new Error('Usuario não é um admin');
+    }
+
+    return this.usersRepository.list();
   }
 }
 
